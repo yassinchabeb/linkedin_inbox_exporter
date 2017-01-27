c14090d9b7081832b2c403a60f519712b3cf1906
Yassin <ychabeb@palo-it.com>
2017-01-27 14:19:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var dates = (document.getElementsByClassName('last-message'));
 var personnes = (document.getElementsByClassName('header'));
 var list="";
 for (var i=0 ; i<dates.length; i++) {
-list = list + "\n" +
+list = list + "\n #[" +
 
 ////////////////////////////  DATES  ///////////////////////////
 (dates[i].getElementsByClassName('timestamp'))[0].innerText
-+ " - " +
++ "] - " +
 //////////////////////////// NAMES OF PERSONS //////////////////
 (personnes[i].getElementsByClassName('name'))[0].innerText
 
